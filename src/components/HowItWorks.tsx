680f35f952a39266f863d0e995e16494ba111457
Plague13 <atarigamesd2@gmail.com>
2025-08-01 13:15:29 -0300

    alteração de texto no HowItWork-2

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -66,7 +66,7 @@ const HowItWorks = () => {
         {/* Depoimento */}
         <div className="bg-white p-6 rounded-lg shadow-lg text-center mb-12 max-w-2xl mx-auto">
           <p className="text-gray-600 text-sm leading-relaxed italic">
-            "Ao acionar a garantia, sempre estaremos prontos para te atender."
+            "Proteja sua familia e fique livre das pragas."
           </p>
           <p className="text-[#f0550e] font-semibold mt-2">Prezamos excelência e qualidade</p>
         </div>
